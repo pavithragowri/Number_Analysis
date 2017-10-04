3c1a9e39354a033742a03c630e8427d39e0c159f
pavithragowri <31505558+pavithragowri@users.noreply.github.com>
2017-10-04 21:01:33 +0530

    Create Odd_Even.java

---
+++ Odd_Even.java
@@ -0,0 +1,1 @@
+import java.util.*;public class Odd_Even{ public static void main(String pavithra[]) {  Scanner in = new Scanner(System.in);  int n = in.nextInt();  if(n%2==0)  {   System.out.println("EVEN");  }  else if(n%2!=0)  {   System.out.println("ODD");  } }}
